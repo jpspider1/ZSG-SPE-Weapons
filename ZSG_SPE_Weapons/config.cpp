0235648761d45cd48abc1c08a0dcdafd43f8b9a9
jpspider1 <jp.spider1@gmail.com>
2025-12-10 16:49:16 -0500

    Add initial config.cpp for ZSG_SPE_Weapons
    Introduces the base configuration file for the ZSG_SPE_Weapons mod, defining CfgPatches with dependencies and placeholders for weapons, magazines, and ammo classes.

---
+++ ZSG_SPE_Weapons/config.cpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+	class ZSG_SPE_Weapons
+	{
+		units[] = {};
+		weapons[] = {""};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"WW2_SPE_Assets_c_Weapons_InfantryWeapons_c"};
+		author = "[23PzD] Jpspider";
+		magazines[] = {};
+		ammo[] = {};
+	};
+};
+
+class CfgWeapons
+{
+
+};
+
+class cfgMagazines
+{
+
+};
+
+class CfgAmmo
+{
+
+};
